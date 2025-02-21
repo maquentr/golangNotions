ba12253bafc509d2c068d7ffe4353f904a2948e7
Matt <matt@Host-006.lan>
2025-02-21 16:54:15 +0100

    reading files

---
+++ reading-files/reading-files.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+    "fmt"
+    "io"
+    "os"
+)
+
+/*Reading files requires checking most calls for errors. This helper will streamline our error checks below.*/
+func check(e error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	
+}
